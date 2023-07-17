4cf1652e6b185e29a1e29f99452338a11b02104e
Wells <ll0918900@gmail.com>
2023-07-17 21:28:29 +0800

    [feat] implement healthy api and test

--- src/BackEnd/Presentation/Program.cs
+++ src/BackEnd/Presentation/Program.cs
@@ -1,27 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+        builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+        var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.Map("/", () => "Hello World");
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapGet("/", () => "Hello World");
-
-app.Run();
---
+++ src/BackEnd/Wsa.GaaS.LittleFriendFighting.UnitTest/EndToEndTestServer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Wsa.GaaS.LittleFriendFighting.UnitTest;
+
+public class EndToEndTestServer : WebApplicationFactory<Program>
+{
+    public HttpClient Client { get; init; }
+
+    public EndToEndTestServer()
+    {
+        Client = CreateClient();
+    }
+}
