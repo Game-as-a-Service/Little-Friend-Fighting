c1ed64db6032c35a8fc4d13e87fcd8641edda367
Wells <ll0918900@gmail.com>
2023-07-23 00:14:26 +0800

    [feat] add first test

---
+++ src/BackEnd/Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+
+namespace Wsa.GaaS.LittleFriendFighting.UnitTest;
+
+public class Tests
+{
+    private EndToEndTestServer _server = null!; 
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        _server = new EndToEndTestServer();
+    }    
+    
+    [Test]
+    public async Task Success()
+    {
+        var response = await _server.Client.GetStringAsync("/");
+        response.Should().Be("Hello World");
+    }
+}
